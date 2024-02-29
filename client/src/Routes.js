59d171e73a33f787e3b94cc3c6792ca9225adbd1
RahulR <rahulramachandran729@gmail.com>
2024-02-29 19:56:36 +0530

    change

--- client/src/Routes.js
+++
@@ -1,5 +0,0 @@
-import LoginPage from "../src/pages/LoginPage";
-import SignupPage from "../src/pages/SignupPage";
-import ActivationPage from "../src/pages/ActivationPage.jsx";
-import HomePage from "./pages/HomePage.jsx";
-export { LoginPage,SignupPage, ActivationPage,HomePage };
